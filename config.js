112ebed2354cfe63ada602c990822b44ab075d93
hrishit <hripat1989@gmail.com>
2019-11-14 00:43:25 +0530

    adding mongo server connnection string for prod environment

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   serverPort: process.env.PORT || 8080,
   dbUrl:
     process.env.NODE_ENV === "production"
-      ? ""
+      ? "mongodb+srv://hrishit:hrishit@cluster0-sopqq.mongodb.net/test?retryWrites=true&w=majority"
       : "mongodb://localhost:27017/postbook",
   secretKey: "Secret key for jwt"
 };
