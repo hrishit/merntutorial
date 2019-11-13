9afea5f390f96395e348356083def778dda54403
hrishit <hripat1989@gmail.com>
2019-11-14 02:32:15 +0530

    mongo connection string experiment

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   serverPort: process.env.PORT || 8080,
   dbUrl:
     process.env.NODE_ENV === "production"
-      ? "mongodb+srv://hrishit:hrishit@cluster0-sopqq.mongodb.net/test?retryWrites=true&w=majority"
+      ? "mongodb://hrishit:hrishit@cluster0-shard-00-00-sopqq.mongodb.net:27017/test"
       : "mongodb://localhost:27017/postbook",
   secretKey: "Secret key for jwt"
 };
